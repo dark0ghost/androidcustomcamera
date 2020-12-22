5b007588d0305dc20a381c846b5a263c649523aa
dark0ghost <dark0ghostworkmail@seznam.cz>
2020-12-22 23:35:05 +0300

    fix

--- app/src/main/java/org/openproject/camera/SettingsActivity.kt
+++ app/src/main/java/org/openproject/camera/SettingsActivity.kt
@@ -2,11 +2,11 @@ package org.openproject.camera
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.widget.ArrayAdapter
 import android.widget.ImageButton
+import android.widget.ListView
 import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.ButtonBarLayout
 
 
 class SettingsActivity: AppCompatActivity() {
@@ -14,18 +14,36 @@ class SettingsActivity: AppCompatActivity() {
     private lateinit var changeModeSavePhotoButton: ToggleButton
     private lateinit var changeViewButton: ImageButton
 
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.setting_layout)
-        changeModeSavePhotoButton = findViewById(R.id.ram_mode)
+        //changeModeSavePhotoButton = findViewById(R.id.ram_mode)
         changeViewButton = findViewById(R.id.change_view)
-        changeModeSavePhotoButton.setOnClickListener {
+        /*changeModeSavePhotoButton.setOnClickListener {
             println("change mode")
-        }
+        }*/
         changeViewButton.setOnClickListener{
-            val intent = Intent(this,MainActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
         supportActionBar?.hide()
+        setListVew()
+    }
+
+    /**
+            <ToggleButton
+        android:id="@+id/ram_mode"
+        android:layout_width="70dp"
+        android:layout_height="77dp" />
+     **/
+    private fun setListVew(){
+        val listView: ListView = findViewById(R.id.listView)
+        val settingsItem: Array<out String> = resources.getStringArray(R.array.ru_text_settings)
+        val adapter:ArrayAdapter<String> = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, settingsItem)
+        listView.adapter = adapter
     }
 }
