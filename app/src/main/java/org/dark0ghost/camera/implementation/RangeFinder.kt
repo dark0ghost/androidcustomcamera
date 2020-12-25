fbbc7f7ee5ac7d84759c123d4ac3436a6c8ee5eb
dark0ghost <dark0ghostworkmail@seznam.cz>
2020-12-25 12:29:40 +0300

    fix

--- app/src/main/java/org/dark0ghost/camera/implementation/RangeFinder.kt
+++ app/src/main/java/org/dark0ghost/camera/implementation/RangeFinder.kt
@@ -1,5 +1,6 @@
 package org.dark0ghost.camera.implementation
 import org.dark0ghost.camera.interface_package.RangeFinder
+import org.opencv.core.Mat
 
 class RangeFinderI: RangeFinder {
     override fun getDistance() {
