7801da8a90878fb103acd69530fb3eeed2c59a3b
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-04-14 19:46:18 +0300

    delete unused code

--- app/src/main/java/org/dark0ghost/camera/implementation/ThreadServer.kt
+++ app/src/main/java/org/dark0ghost/camera/implementation/ThreadServer.kt
@@ -19,45 +19,48 @@ open class ThreadServer(private val trigger: List<String>, openPort: Int, privat
 
     private fun isCommand(message: String): Boolean = message.replace("\n","") in trigger
 
-    private fun runTask(message: String, buffer:  BufferedReader): Unit = when (message){
-        "send"->{
-            Log.e(logTag, "wait callback")
-            val res = callaBack()
-            Log.e(logTag, "send data $res")
-            bufferSender!!.println(res)
-        }
-        "set_focus" ->{
-            Log.e(logTag, "wait number focus")
-            val mes = buffer.readLine()
-            val distances = mes.toFloatOrNull()
-            if (distances != null) {
-                GlobalSettings.manualFocus = distances
-                GlobalSettings.isManualFocus = true
-                Log.e(logTag, "get focus distance $mes")
-                bufferSender!!.println("ok, focus set")
-            }else {
-                bufferSender!!.println("error: $mes is not float")
+    private fun runTask(message: String, buffer:  BufferedReader): Unit {
+        Log.e(logTag, "wait callback")
+        when (message){
+            "send"->{
+                Log.e(logTag, "wait callback")
+                val res = callaBack()
+                Log.e(logTag, "send data $res")
+                bufferSender!!.println(res)
             }
-            updateCamera()
-        }
-        "set_size_photo" ->{
-            Log.e(logTag, "wait size photo")
-            val mes = buffer.readLine()
-            val listSize = mes.split(":")
-            val castToIntFirstSize = listSize[0].toIntOrNull()
-            val castToIntSecondSize = listSize[1].toIntOrNull()
-            if(castToIntFirstSize != null && castToIntSecondSize != null){
-                GlobalSettings.sizePhoto = Size(castToIntFirstSize,castToIntSecondSize)
-                bufferSender!!.println("ok, size photo set")
+            "set_focus" ->{
+                Log.e(logTag, "wait number focus")
+                val mes = buffer.readLine()
+                val distances = mes.toFloatOrNull()
+                if (distances != null) {
+                    GlobalSettings.manualFocus = distances
+                    GlobalSettings.isManualFocus = true
+                    Log.e(logTag, "get focus distance $mes")
+                    bufferSender!!.println("ok, focus set")
+                }else {
+                    bufferSender!!.println("error: $mes is not float")
+                }
                 updateCamera()
-            }else {
-                bufferSender!!.println("error size: $castToIntFirstSize:$castToIntSecondSize")
             }
+            "set_size_photo" ->{
+                Log.e(logTag, "wait size photo")
+                val mes = buffer.readLine()
+                val listSize = mes.split(":")
+                val castToIntFirstSize = listSize[0].toIntOrNull()
+                val castToIntSecondSize = listSize[1].toIntOrNull()
+                if(castToIntFirstSize != null && castToIntSecondSize != null){
+                    GlobalSettings.sizePhoto = Size(castToIntFirstSize,castToIntSecondSize)
+                    bufferSender!!.println("ok, size photo set")
+                    updateCamera()
+                }else {
+                    bufferSender!!.println("error size: $castToIntFirstSize:$castToIntSecondSize")
+                }
+            }
+            "get_focus_data" -> {
+                bufferSender!!.println(cameraInfo?.zoomState?.value.toString())
+            }
+            else -> Unit
         }
-        "get_focus_data" -> {
-            bufferSender!!.println(cameraInfo?.zoomState?.value.toString())
-        }
-        else -> Unit
     }
 
     private fun runServer(){
@@ -66,6 +69,9 @@ open class ThreadServer(private val trigger: List<String>, openPort: Int, privat
         while (!isInterrupted && !serverSocket.isClosed) {
             try {
                 Log.e(logTag, "wait")
+                val inputStreamold = clientSocket.getInputStream()
+                val inputDataold = BufferedReader(InputStreamReader(inputStreamold))
+
                 try {
                     clientSocket = serverSocket.accept()
                 } catch (e: java.net.SocketException) {
@@ -95,6 +101,7 @@ open class ThreadServer(private val trigger: List<String>, openPort: Int, privat
                 printTrace(e)
             }
         }
+        Log.e(logTag, "stop func")
         }
 
     init{
