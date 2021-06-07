f598cec99a3822831b5740687f852e92926258dc
dark0ghost <dark0ghostworkmail@seznam.cz>
2021-06-07 22:46:46 +0300

    fix hash

--- app/src/main/java/org/dark0ghost/camera/consts/Consts.kt
+++ app/src/main/java/org/dark0ghost/camera/consts/Consts.kt
@@ -4,6 +4,7 @@ import android.Manifest
 
 data class ConstVar(var logTag: String = "CameraLogs",var fileNameFormat: String = "yyyy-MM-dd-HH-mm-ss-SSS",val requestCodePermission: Int = 10,val requiredPermissions: Array<String> = arrayOf(
     Manifest.permission.CAMERA),val ramModeTag: String = "ram_mode_tag", val portTag: String = "port_tag",val startServerTag: String = "start_server_tag",val triggerTag: String = "trigger_tag",val isServerStartTag: String = "is_server_start_tag",val isRangeFinderStartTag: String = "is_range_finder_start_tag") {
+    private val hashConst: Int = 31
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -15,9 +16,9 @@ data class ConstVar(var logTag: String = "CameraLogs",var fileNameFormat: String
 
     override fun hashCode(): Int {
         var result = logTag.hashCode()
-        result = 31 * result + fileNameFormat.hashCode()
-        result = 31 * result + requestCodePermission
-        result = 31 * result + requiredPermissions.contentHashCode()
+        result += hashConst * result + fileNameFormat.hashCode()
+        result += hashConst * result + requestCodePermission
+        result += hashConst * result + requiredPermissions.contentHashCode()
         return result
     }
 
